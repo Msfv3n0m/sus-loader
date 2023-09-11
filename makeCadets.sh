8836d7ac3fe49762c0e10ccd127363973e7f81aa
Msfv3n0m <71530214+Msfv3n0m@users.noreply.github.com>
2023-09-11 11:47:31 -0400

    Update makeCadets.sh

--- makeCadets.sh
+++ makeCadets.sh
@@ -10,6 +10,6 @@ do
   cp sus-loader.asm ./tmp/sus-loader.asm
   sed -i "s/Cadet0/$cadet/g" './tmp/sus-loader.asm'
   make buildtmp 
-  mv ./tmp/sus-loader/img "./cadets/$cadet.img"
+  mv ./tmp/sus-loader.img "./cadets/$cadet.img"
 done
 rm -rf ./tmp
